fbfc90e7b2b0c0d9badd157aa02d045c81838725
Naroth123444 <narothlao@gmail.com>
2025-04-02 01:01:57 +0700

    change name file to SidebarComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import NavbarComponent from './components/navbar/NavbarComponent'
+import NavbarComponent from './components/sidebar/SidebarComponent'
 import CounterComponent from './components/counter/CounterComponent'
 import BekMen from './components/counter/BekMen'
 function App() {
