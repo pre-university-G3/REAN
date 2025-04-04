fbd528e408cdb0ac905c9a8ba8417f1a85d0587d
KungSovannda <kungsovannda@gmail.com>
2025-04-05 02:35:53 +0700

    Add 404 file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeaderComponent from "./components/header/HeaderComponent";
 import FooterComponent from "./components/footer/FooterComponent";
+import FileNotFound from "./components/filenotfound/FileNotFound";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<h1>HomePage</h1>} />
         <Route path="/about" element={<h1>About Page</h1>} />
         <Route path="/courses" element={<h1>Courses Page</h1>} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="*" element={<FileNotFound />} />
       </Routes>
       <FooterComponent />
     </BrowserRouter>
---
+++ src/components/filenotfound/FileNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function FileNotFound() {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/d30f843e-36e4-483e-a100-003fa8b6184f/IjA3Mi7NqB.lottie"
+      loop
+      autoplay
+    />
+  );
+}
