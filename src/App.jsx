02b21c11abcd063ee2e9de1ab38dfd454f5b1b8c
KungSovannda <kungsovannda@gmail.com>
2025-04-04 08:41:50 +0700

    Merging

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HeaderComponent from "./components/header/HeaderComponent";
-<<<<<<< HEAD
 import HomePage from "./pages/HomePage";
-=======
 import FooterComponent from "./components/footer/FooterComponent";
->>>>>>> 249c9bd1a34a59600638b3be87e1d686d74b005b
 
 function App() {
   return (
