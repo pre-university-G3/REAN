9bfc54590de37196b6a545fb3edf03273a1ab87f
KungSovannda <kungsovannda@gmail.com>
2025-04-05 09:58:46 +0700

    Update UI

--- src/components/coursedetail/DropDown.jsx
+++ src/components/coursedetail/DropDown.jsx
@@ -2,31 +2,39 @@ import { useState } from "react";
 import { PlayCircle } from "lucide-react";
 import { ChevronUp, ChevronDown } from "lucide-react";
 
-
 const CourseDetail = ({ title, lessons }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <button className={`w-full border border-gray-300 flex items-center justify-between p-4 font-semibold text-sub-title-large rounded-lg ${isOpen ? "bg-yellow-400" : ""}`}
-            onClick={() => setIsOpen(!isOpen)}>
-            {title}
-            {isOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
-        </button>
-        <div className={`transition-all duration-300 ease-in-out ${
+      <button
+        className={`w-full border border-gray-300 flex items-center justify-between p-4 font-semibold text-sub-title-large rounded-lg ${
+          isOpen ? "bg-yellow-400" : ""
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
+        {isOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
+      </button>
+      <div
+        className={`transition-all duration-300 ease-in-out ${
           isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0 overflow-hidden"
-            }`}>
-            {lessons.map((lesson, index) => (
-              <div key={index} className="flex items-center justify-between p-4 border-t bg-gray-50 hover:bg-gray-100">
-                <div className="flex items-center gap-4">
-                  <div className="w-10 h-10 flex items-center justify-center bg-gray-200 rounded-full">
-                    <PlayCircle size={20} className="text-primary" />
-                  </div>
-                  <p className="text-primary">{lesson.title}</p>
-                </div>
-                <span className="text-primary">{lesson.duration}</span>
+        }`}
+      >
+        {lessons.map((lesson, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between p-4 border-t bg-gray-50 hover:bg-gray-100"
+          >
+            <div className="flex items-center gap-4">
+              <div className="w-10 h-10 flex items-center justify-center bg-gray-200 rounded-full">
+                <PlayCircle size={20} className="text-primary" />
               </div>
-            ))}
-        </div>
+              <p className="text-primary">{lesson.title}</p>
+            </div>
+            <span className="text-primary">{lesson.duration}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -35,7 +43,11 @@ const CourseList = () => {
     {
       title: "Introduction",
       lessons: [
-        { title: "Introduction, basics of terminology, construction of web pages", duration: "2h 40m" },
+        {
+          title:
+            "Introduction, basics of terminology, construction of web pages",
+          duration: "2h 40m",
+        },
         { title: "Semantic and Accessibility", duration: "5h 30m" },
       ],
     },
@@ -77,9 +89,13 @@ const CourseList = () => {
   ];
 
   return (
-    <div className="space-y-2 mt-[100px]">
+    <div className="space-y-2">
       {sections.map((section, index) => (
-        <CourseDetail key={index} title={section.title} lessons={section.lessons} />
+        <CourseDetail
+          key={index}
+          title={section.title}
+          lessons={section.lessons}
+        />
       ))}
     </div>
   );
--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -1,63 +1,74 @@
-
-import React from 'react'
+import React from "react";
 import HeroSection from "../components/coursedetail/HeroSection";
-import DropDown from "../components/coursedetail/DropDown"
-import AllCourseCard from "../components/card/AllCourseCard"
+import DropDown from "../components/coursedetail/DropDown";
+import AllCourseCard from "../components/card/AllCourseCard";
 
 export default function CourseDetail() {
   return (
-    <main className='flex flex-col items-center'>
-        <HeroSection />
+    <main className="flex flex-col items-center space-y-[96px] pb-24">
+      {/* Hero Section */}
+      <HeroSection />
 
-        <section className='flex flex-col w-full md:h-[70vh] pt-[50px] px-30'>
-          <h1 className='text-accent text-h2-large font-bold'>
-              Course Content
+      {/* Course Content */}
+      <article className="px-4 md:px-30 space-y-[96px]">
+        <section className="w-full flex flex-col space-y-10">
+          <h1 className="text-accent text-h2-large font-bold">
+            Course Content
           </h1>
-          {/* Cours content */}
           <DropDown />
         </section>
-          {/* Description */}
-        <section className='flex flex-col w-full md:h-[40vh] pt-[50px] px-30'>
-          <h1 className='text-accent text-h2-large font-bold'>
-              Description
-          </h1>
-          <p className='text-primary text-body-text-large mt-10'>
-          It would be best to have a basic understanding of HTML and CSS. If you still need to learn the fundamentals, we link to some excellent resources inside the path. We recommend starting the path and using the resources to boost your knowledge before tackling the first challenge.
+        {/* Description */}
+        <section className="w-full">
+          <h1 className="text-accent text-h2-large font-bold">Description</h1>
+          <p className="text-primary text-body-text-large mt-10">
+            It would be best to have a basic understanding of HTML and CSS. If
+            you still need to learn the fundamentals, we link to some excellent
+            resources inside the path. We recommend starting the path and using
+            the resources to boost your knowledge before tackling the first
+            challenge.
           </p>
         </section>
-          {/* All course */}
-        <section className='flex flex-col w-full md:h-[100vh] pt-[30px] px-30'>
-            <h1 className='text-accent text-h2-large font-bold'>
-                All Course
-            </h1>
-            <div className='mt-5'>
-                <nav className='text-gray-500'>
-                    <a href="#" className='hover:text-accent text-body-text-large'>View More <span className='mx-3'>&gt;</span></a>
-                </nav>
-            </div>
-            <AllCourseCard />
+        {/* All Courses */}
+        <section className="w-full">
+          <h1 className="text-accent text-h2-large font-bold">All Course</h1>
+          <div className="mt-5">
+            <nav className="text-gray-500">
+              <a href="#" className="hover:text-accent text-body-text-large">
+                View More <span className="mx-3">&gt;</span>
+              </a>
+            </nav>
+          </div>
+          <AllCourseCard />
         </section>
-          {/* Instructor */}
-        <section className='flex flex-col w-full md:h-[60vh] pt-[30px] px-30 '>
-                <h1 className='text-accent text-h2-large font-bold'>
-                    Instructor
+        {/* Instructor */}
+        <section className="w-full">
+          <h1 className="text-accent text-h2-large font-bold">Instructor</h1>
+          <article className="bg-white rounded-lg shadow-md border p-10 mt-10 border-black">
+            <div className="flex flex-col md:flex-row gap-5">
+              <figure className="flex-shrink-0">
+                <img
+                  src="https://img-c.udemycdn.com/user/200_H/12013100_e696_6.jpg"
+                  alt=""
+                  className="w-[200px] h-[200px] rounded-full object-cover"
+                />
+              </figure>
+              <div className="flex-grow">
+                <h1 className="text-h3-large text-primary font-bold mb-3">
+                  Joseph Delgadillo
                 </h1>
-            <article className='bg-white rounded-lg shadow-md border p-10 mt-10 border-black'>
-                <div className='flex flex-row gap-5'>
-                    <figure className='flex-shrink-0'>
-                        <img src="https://img-c.udemycdn.com/user/200_H/12013100_e696_6.jpg" alt="" className='w-50 h-50 rounded-full object-cover'/>
-                    </figure>
-                    <div className='flex-grow'>
-                        <h1 className='text-h3-large text-primary font-bold mb-3'>
-                            Joseph Delgadillo
-                        </h1>
-                        <p className='text-body-text-large text-primary mt-2'>
-                        Joseph Delgadillo is the founder of JTD Courses, an education technology company. He has been active in the online learning industry since 2015 and has worked with several prominent companies. As of 2024, over 1 million students across the globe have enrolled in his courses covering network security, Python programming, web development and more.
-                        </p>
-                    </div>
-                </div>  
-            </article>
+                <p className="text-body-text-large text-primary mt-2">
+                  Joseph Delgadillo is the founder of JTD Courses, an education
+                  technology company. He has been active in the online learning
+                  industry since 2015 and has worked with several prominent
+                  companies. As of 2024, over 1 million students across the
+                  globe have enrolled in his courses covering network security,
+                  Python programming, web development and more.
+                </p>
+              </div>
+            </div>
+          </article>
         </section>
+      </article>
     </main>
-  )
+  );
 }
