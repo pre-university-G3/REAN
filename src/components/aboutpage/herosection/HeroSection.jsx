5fb0bb2ce2b1332e3d5d6603e84eab9459370176
KungSovannda <kungsovannda@gmail.com>
2025-04-20 14:54:52 +0700

    fix: fix header in about

--- src/components/aboutpage/herosection/HeroSection.jsx
+++ src/components/aboutpage/herosection/HeroSection.jsx
@@ -44,17 +44,17 @@ export default function HeroSection() {
         <figure className="w-full h-full justify-center md:justify-end items-center overflow-y-visible relative flex">
           <img className="w-[90%]" src={"/img/heroabout.svg"} alt="" />
         </figure>
+        <img
+          className="absolute top-[358px] left-[-148px] z-0 "
+          src={"/img/circle-round.svg"}
+          alt=""
+        />
+        <img
+          className="absolute top-[-100px] translate-x-[250px] lg:translate-x-[800px] z-0 "
+          src={"/img/circle-round.svg"}
+          alt=""
+        />
       </section>
-      <img
-        className="absolute top-[358px] left-[-148px] z-0 "
-        src={"/img/circle-round.svg"}
-        alt=""
-      />
-      <img
-        className="absolute top-[-100px] translate-x-[250px] lg:translate-x-[800px] z-0 "
-        src={"/img/circle-round.svg"}
-        alt=""
-      />
     </>
   );
 }
